576774acc41f966b440b7e04ff7fc0bf7259affe
Troy Sankey <sankeytms@gmail.com>
2018-11-15 13:20:43 -0500

    make more object oriented, add planned support for Cube

--- README.rst
+++ README.rst
@@ -4,30 +4,38 @@ cluttool - create and convert 3D/color LUTs
 This tool is a sort of swiss army knife for color LUTs.  Color LUTs concisely
 describe linear color transformations that many video monitors and post
 production software can understand.  There are a relatively small number of
-color LUT file formats, such as 3D LUT and Hald CLUT, but no one LUT format is
-used by every software and firmware system.  This tool may help you to convert
-between LUT formats, or generate new ones in the format that you need.
+color LUT file formats, such as 3D LUT (3DL) and Hald CLUT, but no one LUT
+format is used by every software and firmware system.  This tool may help you
+to convert between LUT formats, or generate new ones in the format that you
+need.
 
 Not all LUTs are created equal
 ------------------------------
 
+This tool is not aware of the context of the color LUT.  It doesn't know what
+type of source medium it was made for, nor what its goals are.  It simply
+converts the color transformations verbatim.
+
 Those film simulation HaldCLUT files aren't going to apply correctly to s-log
 footage, and those video style LUT packs aren't going to apply corrrectly to
-your 8-bit gamma or 14-bit RAW photos.  You need to know what _source_ the
-color LUT was created for in order to use it.
+your 8-bit gamma or 14-bit RAW photos.  If you're grabbing color LUTs off the
+internet, you should understand what *source* the color LUT was created for,
+and what kind of transformation it does.
 
-This tool is not aware of the context of the color LUT.  It doesn't know
-whether the LUT was made for gamma, log, or linear raw images.  It simply
-converts them verbatim.
+When in doubt, just test it.  However, some LUT packs are purchased and
+non-refundable.  My hope is that this tool will help make it easier for you to
+freely create your own color LUTs for your own needs.
 
-Supported file formats:
+Supported color LUT formats:
 -----------------------
 
-* PNG (.png)
+* Hald CLUT (.png)
 * 3D LUT (.3dl)
+* Cube LUT (.cube)
 
 References:
 -----------
 
 * Hald CLUT reference: http://www.quelsolaar.com/technology/clut.html
 * 3D LUT (3DL) reference: http://download.autodesk.com/us/systemdocs/pdf/lustre_color_management_user_guide.pdf#page=14
+* Cube LUT reference: http://wwwimages.adobe.com/www.adobe.com/content/dam/acom/en/products/speedgrade/cc/pdfs/cube-lut-specification-1.0.pdf
--- cluttool/cluttool.py
+++ cluttool/cluttool.py
@@ -11,7 +11,9 @@
 import sys
 import click
 
-SUPPORTED_BIT_DEPTHS = [8, 10]
+import numbers
+
+SUPPORTED_3DL_BIT_DEPTHS = [8, 10]
 SUPPORTED_IMAGE_FILETYPES = ['png']
 
 # The 3DL format begins with a list of values (i.e. sampling intervals) from
@@ -34,10 +36,6 @@ HEADER_3DL_INTERVALS = {
     10: '0 64 128 192 256 320 384 448 512 576 640 704 768 832 896 960 1023',
 }
 
-# The PMM header describes the width and height of the image in pixels,
-# followed by the maximum color component value.
-HEADER_PPM_FMT = 'P3 {} {} {}'
-
 # The higher this number, the wider the aspect ratio of the identity image.
 # The current value of 4 tends to make the resulting image square when using
 # the 16- or 17-sample headers above.
@@ -58,14 +56,16 @@ def max_value_for_bit_depth(bit_depth):
     return (2**bit_depth)-1
 
 
-def generate_identity_data(bit_depth):
-    if bit_depth not in HEADER_3DL_INTERVALS
+def generate_3dl_identity_data(bit_depth_3dl):
+    if bit_depth_3dl not in HEADER_3DL_INTERVALS
         raise NotImplementedError('{}-bit color depth is not supported'.format(bit_depth))
     intervals = HEADER_3DL_INTERVALS[bit_depth] 
     interval_values = intervals.split()
     interval_count = len(interval_values)
     width = interval_count * IDENTITY_IMAGE_COLUMN_MULTIPLIER
     height = math.ceil(interval_count**3/width)
+    bit_depth_image = None
+    if bit_depth_3dl
     data = []
     for r in interval_values:
         for g in interval_values:
@@ -74,63 +74,113 @@ def generate_identity_data(bit_depth):
     return data, width, height
 
 
-def generate_identity_png(out_filename, bit_depth):
+def write_png(path, data, width, height, bit_depth):
     """
-    Create an "identity" 3DL PNG file.
+    Create an "identity" PNG file.
     """
-    data, width, height = generate_identity_data(bit_depth)
     writer = png.writer(
         width=width,
         height=height,
         bitdepth=bit_depth,
     )
-    writer.write_array(out_filename, data)
-
-@click.group()
-def cli():
-    """
-    This is the top level command, which doesn't do anything on its own.
-    """
-    pass
-
-@cli.command()
-@click.argument(
-    'output_filename',
-    type=click.Path(exists=False)
-)
-@click.option(
-    'bit_depth', '--bit-depth',
-    help='bit depth represented by 3DL identity image',
-    default=SUPPORTED_BIT_DEPTHS[0],
-    type=click.Choice(SUPPORTED_BIT_DEPTHS)
-)
-def identity_image(bit_depth, filetype):
-    """
-    Generate a 3D LUT "identity" image to use with your favorite image editor.
-    """
-    generate_identity_image_func = {
-        'png': generate_identity_png,
-    }[filetype]
-    generate_identity_image_func(out_filename, bit_depth)
+    writer.write_array(path, data)
 
 
-@click.argument(
-    'transformed_image_filename',
+########################################
+
+class ColorLUT(object):
+    """
+    """
+    def __init__(self, data, sample_count=None, input_domain=None, flip_r_b=False):
+        if not isinstance(data, list) or not isinstance(data[0], numbers.Number):
+            raise ValueError('data parameter should be a flat list of numbers.')
+        if not isinstance(sample_count, int):
+            raise ValueError('sample_count parameter should be of type int.')
+        if not isinstance(input_domain, tuple) or not isinstance(input_domain[0], numbers.Number) or len(input_domain) != 3:
+            raise ValueError('input_domain parameter should be a tuple of input domains for all 3 channels.')
+        if not len(data) == 3 * (sample_count**3): 
+            raise ValueError('The sample invervals do not appear to match the matrix dimensions.')
+        self.data = data
+        self.sample_count = sample_count
+        self.input_domain = input_domain
+        if flip_r_b:
+            _flip_r_b()
+
+    def _flip_r_b(self):
+        """
+        Reorder the data values in order to make the red/blue channels
+        increment most/least rapidly, or least/most rapidly, whichever is
+        opposite to the previous state..
+
+        Some Color LUT formats may increment the red channel most rapidly,
+        others the blue channel.  This function helps to convert between the
+        two styles of representing 3D matricies in memory.
+        """
+        data_flipped = array(self.data.typecode)
+        for x in range(self.sample_count):
+            for y in range(self.sample_count):
+                for z in range(self.sample_count):
+                    idx = x + self.sample_count * y + self.sample_count^2 * z
+                    value3D = self.data[idx:idx+3]
+                    data_flipped.extend(value3D)
+        del self.data
+        self.data = data_flipped
+
+    @classmethod
+    def from_haldclut(cls, src):
+        png.Reader(filename=src)
+        width, height, pixels, meta = png.read_flat()
+        if 'palette' in meta:
+            raise ValueError('Then given PNG file uses a color palette. Refusing.')
+        if 'gamma' in meta:
+            raise ValueError('Then given PNG file contains a gamma value. Refusing.')
+        if 'transparent' in meta:
+            raise ValueError('Then given PNG file specifies a transparent color. Refusing.')
+        if meta['alpha']:
+            raise ValueError('Then given PNG file contains an alpha channel. Refusing.')
+        if meta['greyscale']:
+            raise ValueError('Then given PNG file is greyscale. Refusing.')
+        if meta['bitdepth'] not in (8, 16):
+            raise ValueError('Then given PNG file specifies an unsupported bit depth. Refusing.')
+	width_is_power_of_two = (width & (width - 1)) == 0
+        if width != height or not width_is_power_of_two:
+            raise ValueError('The given PNG file does not have appropriate Hald CLUT dimensions. Refusing.')
+        sample_count = int(round((width**2)**(1./3)))
+        input_domain = 2**meta['bitdepth']
+        return cls(data, sample_count, input_domain)
+
+
+@click.command()
+@cli.argument(
+    'src',
     type=click.Path(exists=True, dir_okay=False)
 )
-def lut_from_transformed_image(transformed_image_filename):
-    """
-    Generate a 3D LUT file (.3dl) based on the given transformed image file.
-    """
-    if '.' not in transformed_image_filename:
-        raise ValueError('file type unknown: {}'.format(transformed_image_filename))
-    extension = transformed_image_filename.split('.')[-1]
-    if not extension in SUPPORTED_IMAGE_FILETYPES:
-        raise ValueError('Inappropriate file type: {}'.format(extension))
-    transformed_values_from_image_func = {
-        'png': transformed_values_from_png,
-    }[extension]
-    transformed_values, intervals = transformed_values_from_image_func()
+@click.option(
+    'type', '--dest-type',
+    help='Type of color LUT.',
+    type=click.Choice(['3dl', 'haldclut']),
+    required=True,
+)
+def cli(src, dest_type):
+    if not src:
+        raise ValueError('Please specify a source LUT')
+    ext = src.lower().split('.')[-1]
+    if ext == 'png':
+        clut = ColorLUT.from_haldclut(src)
+    elif ext == '3dl':
+        clut = ColorLUT.from_3dl(src)
+    elif ext == 'cube':
+        clut = ColorLUT.from_haldclut(src)
+    else:
+        raise ValueError('Not an appropriate Color LUT file type: {}'.format(ext))
+    if dest_type == 'haldclut':
+        clut.write_haldclut()
+    elif dest_type == '3dl':
+        clut.write_3dl()
+    elif dest_type == 'cube':
+        clut.write_cube()
+    else:
+        raise ValueError('Not an appropriate Color LUT file type: {}'.format(ext))
 
 
 if __name__ == '__main__':
